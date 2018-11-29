9bba943ddda258985037d3c6c58a7a62eed8240a
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-11-29 14:19:54 +0000

    Updates

---
+++ precache-manifest.709c5a721c18836a8f4cb49952ae9d12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d12536c75b68575b8b7717729e8a0fc",
+    "url": "/my-portfolio/static/media/logo.7d12536c.png"
+  },
+  {
+    "revision": "6fdcd94398404bc9a358c6f1dc2ac36c",
+    "url": "/my-portfolio/static/media/heroPicture.6fdcd943.jpg"
+  },
+  {
+    "revision": "e65bb53fe14813239705536fb4bb153e",
+    "url": "/my-portfolio/static/media/arrow.e65bb53f.png"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "be389b55255ee7feac7e",
+    "url": "/my-portfolio/static/js/main.be389b55.chunk.js"
+  },
+  {
+    "revision": "9039df44b9125bfd1511",
+    "url": "/my-portfolio/static/js/1.9039df44.chunk.js"
+  },
+  {
+    "revision": "be389b55255ee7feac7e",
+    "url": "/my-portfolio/static/css/main.c389ba18.chunk.css"
+  },
+  {
+    "revision": "12e577f01ba6ac3718f4524602b590d4",
+    "url": "/my-portfolio/index.html"
+  }
+];
