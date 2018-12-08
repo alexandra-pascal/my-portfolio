668a030308eae72859d78bcb766fa0e3219fffe0
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-08 18:33:58 +0000

    add font families and text on image

--- src/components/Home/PictureSection.js
+++ src/components/Home/PictureSection.js
@@ -1,11 +1,15 @@
 import React from "react";
-import MainPicture from './../../img/me-green.jpg';
+import MainPicture from "./../../img/me-green.jpg";
 import "./PictureSection.css";
 
 export default function PictureSection() {
   return (
     <div className="picture-section-wrap">
-      <img src={MainPicture} alt="Alexandra Pascal"/>
+      <img src={MainPicture} alt="Alexandra Pascal" />
+      <div className="wrap-hello">
+        <p className="hello">Hello, </p>
+        <p className="hello-subtitle">I'm Alex. Nice to meet you</p>
+      </div>
     </div>
   );
 }
