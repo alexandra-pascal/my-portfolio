b15c0556e7aa7e6dbea3394a7654be6eba98a409
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-08 21:41:02 +0000

    add icons in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,12 @@ import "./Footer.css";
 export default function Toolbar(props) {
   return (
     <div className="footer">
-      <p>footer</p>
+      <div className="footer-icons">
+        <i className="fa fa-github" />
+        <i className="fa fa-linkedin" />
+        <i className="fa fa-behance" />
+      </div>
+      <p>©Handcrafted by me | All rights reserved</p>
     </div>
   );
 }
