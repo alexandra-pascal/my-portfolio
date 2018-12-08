c6115c8012a457105e6163edab4e9c5cc3aa73d7
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-08 22:35:14 +0000

    create routes for all the pages

---
+++ src/components/PortfolioPage/PortfolioPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import './PortfolioPage.css'
+
+export default function PortfolioPage() {
+  return <div>this is Portfolio page</div>;
+}
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./Home/Home";
+import Skills from "./Skills/Skills";
+import Languages from "./Languages/Languages";
+import Portfolio from "./Portfolio/Portfolio";
+import PortfolioPage from "./PortfolioPage/PortfolioPage";
+import Contact from "./Contact/Contact";
 
 export default function Routes() {
   return (
     <div>
       <Route exact path="/" component={Home} />
+      <Route path="/skills" component={Skills} />
+      <Route path="/languages" component={Languages} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/portfolio-page" component={PortfolioPage} />
+      <Route path="/contact" component={Contact} />
     </div>
   );
 }
