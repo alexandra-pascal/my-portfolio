6a4712916ae165c963fc2813cae6f83010956b5d
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-08 18:55:38 +0000

    added text in the description section

--- src/components/Home/DescriptionSection.js
+++ src/components/Home/DescriptionSection.js
@@ -4,15 +4,18 @@ import "./DescriptionSection.css";
 export default function DescriptionSection() {
   return (
     <div className="description-section-wrap">
-      <div>this is description</div>
-      <div>this is description</div>
-      <div>this is description</div>
-      <div>this is description</div>
-      <div>this is description</div>
-      <div>this is description</div>
-      <div>this is description</div>
-      <div>this is description</div>
-      <div>this is description</div>
+      <p className="description-title">Front-end developer</p>
+      <p className="description-title">Designer</p>
+      <p className="description-text">
+        Java Script, consectetur adipisicing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.
+        Duis aute irure dolor.{" "}
+      </p>
+      <p className="description-text">
+        Orem ipsum dolor sit amet, consectetur adipisicing elit tempor
+        incididunt ut.{" "}
+      </p>
     </div>
   );
 }
