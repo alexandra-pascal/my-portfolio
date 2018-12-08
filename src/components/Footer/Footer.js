e8a38ebf5d28d6ad22d5daa8706d6f6daf3080bb
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-08 23:20:40 +0000

    add links to the icons in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,15 @@ export default function Toolbar(props) {
   return (
     <div className="footer">
       <div className="footer-icons">
-        <i className="fa fa-github" />
-        <i className="fa fa-linkedin" />
-        <i className="fa fa-behance" />
+        <a href="https://github.com/alexandra-pascal?tab=repositories" target="_blank" rel='noreferrer noopener'>
+          <i className="fa fa-github" />
+        </a>
+        <a href="https://www.linkedin.com/in/alexandra-pascal-7354b4127/" target="_blank" rel='noreferrer noopener'>
+          <i className="fa fa-linkedin" />
+        </a>
+        <a href="https://www.behance.net/alexandrap6ffd" target="_blank" rel='noreferrer noopener'>
+          <i className="fa fa-behance" />
+        </a>
       </div>
       <p>©Handcrafted by me | All rights reserved</p>
     </div>
