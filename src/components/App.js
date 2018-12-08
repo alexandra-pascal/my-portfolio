d146ddbf242bbd482c3f43c85d0edbeafcbb06e5
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-08 12:13:20 +0000

    start over

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import Home from "./Home";
-import Portfolio from "./Portfolio";
-import "../css/App.css";
+import { HashRouter as Router } from "react-router-dom";
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Route exact path="/" component={Home} />
-          <Route path="/portfolio" component={Portfolio} />
+          <p>My portfolio</p>
         </div>
       </Router>
     );
