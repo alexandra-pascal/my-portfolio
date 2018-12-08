19847f6aeaab337df31210557a5a7530083a31a8
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-08 17:18:32 +0000

    add logo

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DrawerToggleButton from "./../SideDrawer/DrawerToggleButton";
+import Logo from "./../../img/logo.svg";
 import "./Toolbar.css";
 
 export default function Toolbar(props) {
@@ -7,7 +8,9 @@ export default function Toolbar(props) {
     <header className="toolbar">
       <nav className="toolbar__navigation">
         <div className="toolbar__logo">
-          <a href="/">LOGO</a>
+          <a href="/">
+            <img src={Logo} alt="logo"/>
+          </a>
         </div>
         <div>
           <DrawerToggleButton click={props.drawerClickHandler}/>
