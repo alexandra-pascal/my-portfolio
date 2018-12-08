3dfc6408049acf53bb51fc12cc2a23a0d441ed9e
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-08 12:21:56 +0000

    single quotes in imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
 
 export default class App extends React.Component {
   render() {
