8eb84b3d3e089aaa8886246f4e6d9fbba1a5d590
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-14 01:17:04 +0000

    hover effect on button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,7 @@ import "./Button.css";
 export default function Button(props) {
   return (
     <Link to="/portfolio">
-      <button className="button">{props.buttonText}</button>
+      <button className="button">{props.buttonText}</button>;
     </Link>
   );
 }
