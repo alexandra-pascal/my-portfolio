e8f36b65e6c616d7de0c308558db2c1c9769ca61
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-16 00:38:34 +0000

    create data for portfolio page

---
+++ src/data/dataPortfolio.js
@@ -0,0 +1,42 @@
+import me from "./../img/me.jpg";
+
+let Projects = [
+  {
+    title: "Kodflix",
+    img: me,
+    description: [
+      "Role: Web Development",
+      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
+      "Styling: CSS"
+    ]
+  },
+  {
+    title: "Seasonal Cuisine",
+    img: me,
+    description: [
+      "Role: Web Development",
+      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
+      "Styling: CSS"
+    ]
+  },
+  {
+    title: "Gentle Dental Care",
+    img: me,
+    description: [
+      "Role: Web Development",
+      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
+      "Styling: CSS"
+    ]
+  },
+  {
+    title: "Digital Bee Line",
+    img: me,
+    description: [
+      "Role: Web Development",
+      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
+      "Styling: CSS"
+    ]
+  }
+];
+
+export default Projects;
