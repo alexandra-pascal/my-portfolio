1765b32fd176f5c3e829069549e360ea04f1149f
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-16 21:58:20 +0000

    add the switch component in Routes.js and create a path to project page

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./Home/Home";
 import Skills from "./Skills/Skills";
 import Languages from "./Languages/Languages";
 import Portfolio from "./Portfolio/Portfolio";
-import PortfolioPage from "./PortfolioPage/PortfolioPage";
 import Contact from "./Contact/Contact";
+import ProjectPage from "./ProjectPage/ProjectPage";
 
 export default function Routes() {
   return (
     <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/skills" component={Skills} />
-      <Route path="/languages" component={Languages} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/portfolio-page" component={PortfolioPage} />
-      <Route path="/contact" component={Contact} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/languages" component={Languages} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/:coverId" component={ProjectPage} />
+      </Switch>
     </div>
   );
 }
