52bf162b17e922b7c66624ad12260b7c52e62a3e
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-16 18:01:57 +0000

    add real description for the projects

--- src/data/dataPortfolio.js
+++ src/data/dataPortfolio.js
@@ -9,10 +9,8 @@ let Projects = [
     img: kodflix,
     description: [
       "Role: Web Development",
-      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
-      "Styling: CSS",
-      "Role: Web Development",
-      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
+      "Description: A full stack project simulating the production of Netflix",
+      "Used technologies: React.js, Node.js, Express and Mongo.DB",
       "Styling: CSS"
     ]
   },
@@ -20,36 +18,28 @@ let Projects = [
     title: "Seasonal Cuisine",
     img: sc,
     description: [
-      "Role: Web Development",
-      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
+      "Role: UI/UX/ Web Development",
+      "Description: Front-end project for a cuisine app where the user can filter their favourite dish for Christmas dinner from several recipes from different countries, types of food and dietary restrictions",
+      "Used technologies: React.js",
       "Styling: CSS",
-      "Role: Web Development",
-      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
-      "Styling: CSS"
     ]
   },
   {
     title: "Gentle Dental Care",
     img: gdc,
     description: [
-      "Role: Web Development",
-      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
-      "Styling: CSS",
-      "Role: Web Development",
-      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
-      "Styling: CSS"
+      "Role: Branding / UI/UX/ Web Development",
+      "Description: A static website, presenting the image of Gentle Dental Care practice online",
+      "Used technologies: Vue.js",
+      "Styling: SCSS",
     ]
   },
   {
     title: "Digital Bee Line",
     img: dbl,
     description: [
-      "Role: Web Development",
-      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
-      "Styling: CSS",
-      "Role: Web Development",
-      "Used technologies: React.js, Node.js, Express and Mongo.DB.",
-      "Styling: CSS"
+      "Role: UX/UI",
+      "Description: A static website, created for a small Creative Digital Agency where the main provided services are web design and web development",
     ]
   }
 ];
