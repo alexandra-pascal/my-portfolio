c4afcf46cc1abcf3a73da888c20d6dc01be15469
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-16 18:28:56 +0000

    remove the background image from portfolio page

--- src/components/Portfolio/ProjectSection.js
+++ src/components/Portfolio/ProjectSection.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Background from "./../../img/background.jpg";
 import "./ProjectSection.css";
 
 export default function Portfolio(props) {
   return (
     <div className="wrap-project">
-    <img src={Background} alt="hero-mobile" className="backgroung-portfolio"/>
       <div className="wrap-img-title">
         <div className="project-title">{props.title}</div>
         <div className="wrap-image-project">
