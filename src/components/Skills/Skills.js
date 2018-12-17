40a891acdabf2f12df52832fe49770d75329427a
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-17 17:22:15 +0000

    create skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,15 @@
 import React from "react";
+import VerticalLine from "./../VerticalLine/VerticalLine";
+import FirstSectionSkills from "./FirstSectionSkills";
+import SecondSectionSkills from "./SecondSectionSkills";
 import './Skills.css'
 
 export default function Skills() {
-  return <div>this is skills</div>;
+  return (
+    <div className="skills-wrap">
+      <FirstSectionSkills />
+      <VerticalLine />
+      <SecondSectionSkills />
+    </div>
+  )
 }
