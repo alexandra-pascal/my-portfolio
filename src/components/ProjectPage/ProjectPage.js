7907961a9fe7f5a22e1ce5147a1f875c0610afac
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-23 17:14:25 +0000

    use the URL parameters for the project pages

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -1,9 +1,38 @@
 import React from "react";
+import Projects from "./../../data/dataPortfolio";
 
-export default function ProjectPage() {
-  return (
-    <div>
-      this is project page
-    </div>
-  );
+export default class ProjectPage extends React.Component {
+  constructor() {
+    super();
+    this.state = { project: {} };
+  }
+
+  componentDidMount() {
+    let projectData = Projects.find(
+      project => project.id === this.props.match.params.coverId
+    );
+    this.setState({ project: projectData });
+  }
+
+  render() {
+    let project3 = this.state.project;
+    return (
+      <div className="wrap-project">
+        <div className="first-section-project">
+          <div className="img">
+            <picture>
+              <source
+                media="(min-width: 500px)"
+                srcSet={project3.imgBig}
+                type="image/jpg"
+              />
+              <img src={project3.imgSmall} alt="project" />
+            </picture>
+          </div>
+        </div>
+        <div className="second-section-project">
+        </div>
+      </div>
+    );
+  }
 }
