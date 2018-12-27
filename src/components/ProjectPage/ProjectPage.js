17f2e9287529d0ce4bccb248cb775fa962f6e6ce
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-27 23:03:07 +0000

    create first section of the project page

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Projects from "./../../data/dataPortfolio";
+import "./ProjectPage.css";
 
 export default class ProjectPage extends React.Component {
   constructor() {
     super();
-    this.state = { project: {} };
+    this.state = {
+      project: {
+        projectDescription: []
+      }
+    };
   }
 
   componentDidMount() {
@@ -19,19 +24,20 @@ export default class ProjectPage extends React.Component {
     return (
       <div className="wrap-project">
         <div className="first-section-project">
+          <div className="project-title2">Gentle Dental Care</div>
           <div className="img">
-            <picture>
-              <source
-                media="(min-width: 500px)"
-                srcSet={project3.imgBig}
-                type="image/jpg"
-              />
-              <img src={project3.imgSmall} alt="project" />
-            </picture>
+            <img src={project3.mockupMob} alt="project" />
+          </div>
+
+          <div className="project-description">
+            {project3.projectDescription.map((eachDescription, index) => (
+              <li key={index} className="li-description">
+                {eachDescription}
+              </li>
+            ))}
           </div>
         </div>
-        <div className="second-section-project">
-        </div>
+        <div className="second-section-project"></div>
       </div>
     );
   }
