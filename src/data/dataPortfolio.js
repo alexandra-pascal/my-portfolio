18000fa69279715453f737482ad2cc5c8b744987
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-27 23:02:03 +0000

    add test text for project description

--- src/data/dataPortfolio.js
+++ src/data/dataPortfolio.js
@@ -6,6 +6,7 @@ import sc from "./../img/sc.jpg";
 import scSmall from "./../img/sc-small.jpg";
 import lds from "./../img/lds.jpg";
 import ldsSmall from "./../img/lds-small.jpg";
+import mobGdc from "./../img/mob-gdc.png";
 
 let Projects = [
   {
@@ -19,6 +20,11 @@ let Projects = [
       "Description: A static website, fully responsive, presenting the image of Gentle Dental Care practice online",
       "Used technologies: Vue.js",
       "Styling: SCSS"
+    ],
+    mockupMob: mobGdc,
+    projectDescription: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable"
     ]
   },
   {
@@ -32,6 +38,11 @@ let Projects = [
       "Description: Front-end-development project for a cuisine app where the user can filter their favourite dish for Christmas dinner from several recipes from different countries, types of food and dietary restrictions",
       "Used technologies: React.js",
       "Styling: CSS"
+    ],
+    mockupMob: mobGdc,
+    projectDescription: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators"
     ]
   },
   {
@@ -45,6 +56,11 @@ let Projects = [
       "Description: A full stack project simulating the production of Netflix",
       "Used technologies: React.js, Node.js, Express and Mongo.DB",
       "Styling: CSS"
+    ],
+    mockupMob: mobGdc,
+    projectDescription: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators"
     ]
   },
   {
@@ -56,6 +72,11 @@ let Projects = [
     description: [
       "Role: Branding / UX/UI",
       "Description: A mobile first application presentating the London Dental Specialists dental practice"
+    ],
+    mockupMob: mobGdc,
+    projectDescription: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators"
     ]
   }
 ];
