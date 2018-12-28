45ba8dec1b4da766fac8dd5ee71cc941c508410d
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-28 22:41:03 +0000

    create layout for the project page

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -7,7 +7,8 @@ export default class ProjectPage extends React.Component {
     super();
     this.state = {
       project: {
-        projectDescription: []
+        projectDescription: [],
+        desktopDescription: []
       }
     };
   }
@@ -22,21 +23,41 @@ export default class ProjectPage extends React.Component {
   render() {
     let project3 = this.state.project;
     return (
-      <div className="wrap-project">
-        <div className="first-section-project">
-          <div className="project-title2">{project3.title}</div>
-          <div className="img">
-            <img src={project3.mockupMob} alt="project" />
+      <div>
+        <div className="wrap-project">
+          <div className="first-section-project">
+            <div className="mob-mockup">
+              <div className="project-title2">{project3.title}</div>
+              <div className="img">
+                <img src={project3.mockupMob} alt="project" />
+              </div>
+              <div className="project-description">
+                {project3.projectDescription.map((eachDescription, index) => (
+                  <li key={index}>{eachDescription}</li>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="project-description">
-            {project3.projectDescription.map((eachDescription, index) => (
-              <li key={index} className="li-description">
-                {eachDescription}
-              </li>
-            ))}
+          <div className="second-section-project" />
+        </div>
+        <div className="wrap-project-desktop">
+          <div className="first-section-project">
+            <div className="mob-mockup">
+              <div className="img-desktop">
+                <img src={project3.mockupDesk} alt="project" />
+              </div>
+            </div>
+          </div>
+          <div className="second-section-project">
+            <div className="description">
+              {project3.desktopDescription.map((eachDescription, index) => (
+                <ul key={index}>
+                  <li>{eachDescription}</li>
+                </ul>
+              ))}
+            </div>
           </div>
         </div>
-        <div className="second-section-project"></div>
       </div>
     );
   }
