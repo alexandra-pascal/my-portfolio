fad5940756ada8f05d80a2a7b79708a7089f7e29
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2019-01-03 00:55:40 +0000

    try with normal menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { slide as Menu } from "react-burger-menu";
+// import { slide as Menu } from "react-burger-menu";
 import Logo from "./../../img/logo.svg";
 import "./Header.css";
 
 export default function Header(props) {
   return (
     <header className="header">
-        <div className="header__logo">
-          <Link to="/">
-            <img src={Logo} alt="logo" />
-          </Link>
-        </div>
-        <Menu right>
+      <div className="header__logo">
+        <Link to="/">
+          <img src={Logo} alt="logo" />
+        </Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li>
+      </ul>
+      {/* <Menu right>
           <a href="/" className="menu-link">
             Home
           </a>
@@ -25,7 +36,7 @@ export default function Header(props) {
           <a href="/contact" className="menu-link">
             Contact
           </a>
-        </Menu>
+        </Menu> */}
     </header>
   );
 }
